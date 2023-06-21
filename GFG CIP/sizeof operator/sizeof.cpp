07c0cccc6ec169949d296b8ebaf01fa9242c6790
Pranjal <tataisarkar2001@gmail.com>
2023-06-21 17:07:03 +0530

    added sizeof.cpp

---
+++ GFG CIP/sizeof operator/sizeof.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<sizeof(int)<<endl;
+    cout<<sizeof(char)<<endl;
+    cout<<sizeof(long)<<endl;
+    cout<<sizeof(double)<<endl;
+
+    return 0;
+}
