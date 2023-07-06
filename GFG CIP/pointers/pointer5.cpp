5a755a450812e2e0a4c71fbd32f2bd2d6d05ff16
Pranjal <tataisarkar2001@gmail.com>
2023-07-06 12:51:46 +0530

    added pointer5.cpp

---
+++ GFG CIP/pointers/pointer5.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    char arr[] = {'g', 'f', 'g'};
+    char *ptr = arr;
+
+    cout<<sizeof(arr)<<endl;
+    cout<<sizeof(ptr);
+
+    return 0;
+}
