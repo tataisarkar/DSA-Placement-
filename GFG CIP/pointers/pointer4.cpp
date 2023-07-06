0c85346ea45bad8d61539ddfd62d74a54372f878
Pranjal <tataisarkar2001@gmail.com>
2023-07-06 12:20:52 +0530

    added pointer4.cpp

---
+++ GFG CIP/pointers/pointer4.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[] = {10, 20, 30};
+    int *ptr = arr;
+
+    cout<<sizeof(arr)<<endl;
+    cout<<sizeof(ptr);
+
+    return 0;
+}
