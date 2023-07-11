0d01d2d7eef4bf39f03969e2344b2dc4f743dd8e
Pranjal <tataisarkar2001@gmail.com>
2023-07-11 12:44:27 +0530

    added structure with a typedef

--- GFG CIP/Structure/structure1.cpp
+++ GFG CIP/Structure/structure1.cpp
@@ -1,18 +1,20 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 struct Point{
     int x;
     int y;
-};
+}P;
 
 int main(){
-    Point P;
+    // P hello;
 
     P.x = 10;
     P.y = 20;
 
     cout<<P.x<< " "<< P.y;
+
+    cout<<typeof(P);
 
     return 0;
 }
---
+++ GFG CIP/Structure/typedefStructure.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef struct Point{
+    int x;
+    int y;
+}P;
+
+int main(){
+    P hello;
+
+    hello.x = 10;
+    hello.y = 20;
+
+    cout<<hello.x<< " "<< hello.y;
+
+    return 0;
+}
